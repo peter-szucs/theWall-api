5926333fadfa7993676adeaf9465926346fef7b6
Peter Szücs <pepo_szucs@hotmail.com>
2020-09-21 17:28:17 +0200

    init SwiftUI
    initialize SwiftUI project

---
+++ theWall/theWall/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  theWall
+//
+//  Created by Peter Szücs on 2020-09-21.
+//  Copyright © 2020 Peter Szücs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
